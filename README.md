691f7e5a425f3b0c38ea66be733808943f50eeee
alejuliao <alexand.juliao@gmail.com>
2021-02-10 11:01:50 -0300

    cleaning dir

---
+++ README.md
@@ -0,0 +1,19 @@
+# Gerador QrCode Pix
+ 
+
+#Sobre o projeto
+ Gerar QrCode estático paga pagamento bancário via Pix em JavaScript.
+ Utilizado padrão QrCode fornecido pelo Banco Central
+## Layout
+
+
+# Tecnologias Utilizadas
+- HTML
+- CSS
+- JAVASCRIPT
+
+# Autor
+
+Alexandre Julião
+
+https://www.linkedin.com/in/alexandrejuliao
---
+++ qrcode.js
@@ -0,0 +1,22 @@
+
+// Codigo funcionando, no segundo click 
+function gqcode(){
+    document.getElementById('linhaCodigo').style.display = "none"
+
+
+    document.getElementById('button')
+        .addEventListener('click', ()=>{
+            const divQr = document.getElementById('qrcode')
+            divQr.innerHTML = ''
+            document.getElementById('linhaCodigo').style.display = "initial"
+            const txid = document.getElementById('linhaCodigo').value
+            const code = new QRCode(divQr, {text:txid, width:300, height:300})
+    })
+}
+//Codigo funcionando, no segundo click 
+
+
+gqcode()
+
+
+
