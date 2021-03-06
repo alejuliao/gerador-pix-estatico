537f5b8846d21f6b101551bcaad9d1a708cd4ecf
alejuliao <alexand.juliao@gmail.com>
2021-03-06 01:15:45 -0300

    validation, amount, css/layout

--- script.js
+++ script.js
@@ -1,117 +1,113 @@
 // PAYLOAD  JS #####################
 const Payload = {
-  // constructor(){
-  // this.id_PAYLOAD_FORMAT_INDICATOR = '00';
-  id_payload_format_indicator: '00',
-  //merchant
-  id_merchant_account_information: '26',
-  id_merchant_account_information_gui: '00',
-  id_merchant_account_information_key: '01',
-  id_merchant_account_information_description: '02',
+  id_payload_format_indicator: "00",
 
-  id_merchant_category_code: '52',
-  id_transaction_currency: '53',
-  id_transaction_amount: '54',
+  id_merchant_account_information: "26",
+  id_merchant_account_information_gui: "00",
+  id_merchant_account_information_key: "01",
+  id_merchant_account_information_description: "02",
 
-  id_country_code: '58',
+  id_merchant_category_code: "52",
+  id_transaction_currency: "53",
+  id_transaction_amount: "54",
 
-  id_merchant_name: '59',
-  id_merchant_city: '60',
+  id_country_code: "58",
 
-  id_addtional_data_field_template: '62',
-  id_additional_data_field_template_txid: '05',
+  id_merchant_name: "59",
+  id_merchant_city: "60",
 
-  id_crc16: '63',
+  id_addtional_data_field_template: "62",
+  id_additional_data_field_template_txid: "05",
 
+  id_crc16: "63",
 
-
-
-
-
-  // this.pixkey = ''
-  // this.description = '';
-  // this.merchantName = ''
-  // this.merchantCity = ''
-  // this.txid = ''
-  // this.amount = ''
-  // this.payload = ''
-  // },
-  // return console.log('a key digitada foi ' + key )
   setPixKey(key) {
-    this.pixkey = key
-    return key
+    this.pixkey = key;
+    return key;
   },
   setDescription(description) {
-    this.description = description
-    return description
+    this.description = description;
+    return description;
   },
   setMerchantName(merchantName) {
-    this.merchantName = merchantName
-    return merchantName
+    this.merchantName = merchantName;
+    return merchantName;
   },
   setMerchantCity(merchantCity) {
-    this.merchantCity = merchantCity
-    return merchantCity
+    this.merchantCity = merchantCity;
+    return merchantCity;
   },
   setTxid(txid) {
-    this.txid = txid
-    return txid
+    this.txid = txid;
+    return txid;
   },
   setAmount(amount) {
-    this.amount = amount
-    return amount
+    this.amount = amount;
+    return amount;
   },
-
 
   getValue(id, value) {
     //fazer o calculo para contar os caracteres, se for menor que 2 adicionar o zero antes
-    var size = value.length
+    var size = value.length;
     if (size < 10) {
-      size = '0' + size
+      size = "0" + size;
     }
-    return id + size + value
+    return id + size + value;
   },
 
   getMerchantAccountInformation(gui, key, description) {
-    gui = this.getValue(this.id_merchant_account_information_gui, 'br.gov.bcb.pix ')
-    key = this.getValue(this.id_merchant_account_information_key, this.pixkey)
-    description = this.getValue(this.id_merchant_account_information_description, this.description)
-    return this.getValue(this.id_merchant_account_information, (gui + key + description))
+    gui = this.getValue(
+      this.id_merchant_account_information_gui,
+      "br.gov.bcb.pix "
+    );
+    key = this.getValue(this.id_merchant_account_information_key, this.pixkey);
+    description = this.getValue(
+      this.id_merchant_account_information_description,
+      this.description
+    );
+    return this.getValue(
+      this.id_merchant_account_information,
+      gui + key + description
+    );
   },
 
   getAdditionalDataFieldTemplate() {
-    var txid = this.getValue(this.id_additional_data_field_template_txid, this.txid)
-    return this.getValue(this.id_additional_data_field_template_txid, txid)
+    var txid = this.getValue(
+      this.id_additional_data_field_template_txid,
+      this.txid
+    );
+    return this.getValue(this.id_additional_data_field_template_txid, txid);
   },
 
   getPayload() {
-    this.payload = this.getValue(this.id_payload_format_indicator, '01')
-      + this.getMerchantAccountInformation()
-      + this.getValue(this.id_merchant_category_code, '0000')
-      + this.getValue(this.id_transaction_currency, '986')
-      + this.getValue(this.id_transaction_amount, '')
-      + this.getValue(this.id_country_code, 'BR')
-      + this.getValue(this.id_merchant_name, this.merchantName)
-      + this.getValue(this.id_merchant_city, this.merchantCity)
-      + this.getAdditionalDataFieldTemplate()
+    this.payload =
+      this.getValue(this.id_payload_format_indicator, "01") +
+      this.getMerchantAccountInformation() +
+      this.getValue(this.id_merchant_category_code, "0000") +
+      this.getValue(this.id_transaction_currency, "986") +
+      this.getValue(this.id_transaction_amount, this.amount) +
+      this.getValue(this.id_country_code, "BR") +
+      this.getValue(this.id_merchant_name, this.merchantName) +
+      this.getValue(this.id_merchant_city, this.merchantCity) +
+      this.getAdditionalDataFieldTemplate();
 
-    return this.payload + this.getCRC16(this.payload)
+    return this.payload + this.getCRC16(this.payload);
   },
 
   getCRC16() {
-    var payload = this.id_crc16 + '04';
+    var payload = this.id_crc16 + "04";
 
     //DADOS DEFINIDOS PELO BACEN
     var polinomio = 0x1021;
-    var resultado = 0xFFFF;
+    var resultado = 0xffff;
 
     //CHECKSUM
-    if (length = payload.length > 0) {
+    if ((length = payload.length > 0)) {
       for (let offset = 0; offset < length; offset++) {
-        resultado ^ (payload.charCodeAt[offset] < 8)
+        resultado ^ (payload.charCodeAt[offset] < 8);
         for (let bitwise = 0; bitwise < 8; bitwise++) {
           if ((resultado <<= 1) & 0x10000) resultado ^ polinomio;
-          resultado &= 0xFFFF;
+          resultado &= 0xffff;
         }
       }
     }
@@ -128,53 +124,89 @@ const Payload = {
       // *     example 3: dechex(-1415723993);
       // *     returns 3: 'ab9dc427'
       if (number < 0) {
-        number = 0xFFFFFFFF + number + 1;
+        number = 0xffffffff + number + 1;
       }
       return parseInt(number, 10).toString(16);
     }
     //RETORNA CÓDIGO CRC16 DE 4 CARACTERES
-    return this.id_crc16 + '04' + payload.toUpperCase() + dechex(resultado)
+    return this.id_crc16 + "04" + payload.toUpperCase() + dechex(resultado);
     //   .dechex();
-  }
+  },
+};
+
+
+const validation = () =>{
+    var select = document.getElementById('options');
+    
+    var typePix = select.options[select.selectedIndex].id;
+
+    
+    const pixkey = document.getElementById("chavepix");
+    pixkey.value = ''
+    if(typePix === 'cpf'){
+        pixkey.setAttribute("maxlength", 11)
+        
+    }else if(typePix === 'celular'){
+        pixkey.setAttribute("maxlength", 15)
+        pixkey.value += '+55'
+    }else if(typePix === 'aleatoria'){
+        
+    }
 
 }
+
+
+
 
 // APP JS #################################
 
-const objPayload = Payload
 
 
-objPayload.setPixKey('')
-objPayload.setDescription('')
-objPayload.setMerchantName('')
-objPayload.setMerchantCity('')
-objPayload.setTxid('')
-objPayload.setAmount()
+
+const objPayload = Payload;
+
+const informations = () => {    
+    const merchant = document.getElementById('merchant').value;
+    const description = document.getElementById('description').value;
+    const amount = document.getElementById('amount').value;
+    objPayload.setPixKey("");
+    objPayload.setMerchantName(merchant);
+    objPayload.setDescription(description);
+    objPayload.setMerchantCity("");
+    objPayload.setTxid("");
+    objPayload.setAmount(amount);
+
+}
+
 
 const Form = {
-  submit(event) {
-    event.preventDefault()
-    codePix()
-  }
-}
+    submit(event) {
+        event.preventDefault();
+        informations()
+        codePix();
+  },
+};
 
-var linhaDeCodigo = document.getElementById('linhaCodigo')
-var pixkey = document.getElementById('chavepix').value
+var linhaDeCodigo = document.getElementById("linhaCodigo");
 
 const codePix = () => {
-  objPayload.setPixKey(pixkey)
+  const pixkey = document.getElementById("chavepix").value;
 
-  const pix = objPayload.getPayload()
+  objPayload.setPixKey(pixkey);
 
-  linhaDeCodigo.innerHTML = pix
+  const pix = objPayload.getPayload();
 
-  createQrCode()
-}
+  linhaDeCodigo.innerHTML = pix;
+
+  createQrCode();
+};
 
 const createQrCode = () => {
-  const divQr = document.getElementById('qrcode')
-  divQr.innerHTML = ''
-  
-  const txid = linhaDeCodigo.textContent
-  const code = new QRCode(divQr, { text: txid, width: 300, height: 300 })
-}
+  const divQr = document.getElementById("qrcode");
+  divQr.innerHTML = "";
+  const pix = linhaDeCodigo.textContent;
+  const code = new QRCode(divQr, { text: pix, width: 300, height: 300 });
+};
+
+
+
